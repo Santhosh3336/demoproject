d43f9a0480e178e395d9656d577d24baab24e4af
Santhosh3336 <abde860santhosh@gmail.com>
2022-09-22 15:57:37 +0530

    new code added

---
+++ src/org/amazon/CameraShop.java
@@ -0,0 +1,10 @@
+package org.amazon;
+
+public class CameraShop {
+       public void shopCamera() {
+		// TODO Auto-generated method stub
+System.out.println("enter the shopcamera");
+
+	}
+}
+
---
+++ src/org/amazon/GoPro.java
@@ -0,0 +1,27 @@
+package org.amazon;
+
+public class GoPro {
+    private void digitalCamera() {
+		// TODO Auto-generated method stub
+    	System.out.println("enter the digitalcamera");
+	}
+	
+	private void cameraLens() {
+		System.out.println("enter the cameralens");
+		// TODO Auto-generated method stub
+
+	}
+	public static void main(String[] args) {
+		GoPro san=new GoPro ();
+		san.digitalCamera();
+		san.cameraLens();
+		
+    CameraShop sen= new CameraShop();
+    
+    sen.shopCamera();
+	}
+
+
+		
+	}
+
